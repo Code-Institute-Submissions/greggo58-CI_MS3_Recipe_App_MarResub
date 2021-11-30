3f033925421e6c1e758fddc21e22b5eaf4cf56ce
greggo58 <greggo58@gmail.com>
2021-11-30 05:03:10 +0000

    README updates debug mode off wireframes

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def delete_profile(username):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
